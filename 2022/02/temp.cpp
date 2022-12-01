19f935e0b16e965798864d765fc85cb1ac0dc170
Max Mogren <maxuno@live.se>
2022-12-01 16:02:28 +0100

    Prep for 2nd challenge

---
+++ 2022/02/temp.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <list>
+#include <vector>
+
+int main(){
+    ifstream file;
+    file.open("input02.txt");
+    string line;
+
+    int most_calories{0};
+
+    vector<int> elfs;
+
+    while(getline(file, line)){
+    }
+}
